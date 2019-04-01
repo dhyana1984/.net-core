0ff80589aaad510045c4d9a5c0e6c7c4a5912341
dhyana1984 <xiongyi_1984@foxmail.com>
2019-04-02 01:57:51 +0800

    Razor页面，增删改查

---
+++ MvcMovie/MvcMovie/Pages/Index.cshtml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MvcMovie.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly MvcMovie.Models.MvcMovieContext _context;
+
+        public IndexModel(MvcMovie.Models.MvcMovieContext context)
+        {
+            _context = context;
+        }
+        //在Razor页面中没有ViewBag
+        //属性上加了ViewData，可以在_Layout中以ViewData["TestProperty"]的方式读取数据
+        [ViewData]
+        public string TestProperty { get; } = "Test";
+
+        //[TempData] 是 ASP.NET Core 2.0 中的新属性，在控制器和页面上受支持
+        //页面模型将 [TempData] 属性应用到 Message 属性。
+        [TempData]
+        public string Message { get; set; }
+
+        public IList<Movie> Movies;
+        public SelectList Genres;
+        //[BindProperty] 特性来选择加入模型绑定。在页面上可以直接用此属性绑定，否则不会绑定
+        [BindProperty]
+        public string MovieGenre { get; set; }
+        [BindProperty]
+        public string SearchString { get; set; }
+  
+        [BindProperty]
+        public MovieGenreViewModel MV { get; set; }
+      
+        public async Task OnGetAsync()
+        {
+
+            Movies = await _context.Movie.ToListAsync();
+            Message = Movies.Count.ToString();
+            IQueryable<string> genreQuery = from m in _context.Movie orderby m.Genre select m.Genre;
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+        }
+
+        public async Task  OnPostSearchMovieAsync()
+        {
+         
+            var movies = from m in _context.Movie select m;
+            
+            //根据关键字和电影流派检索
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                movies = movies.Where(t => t.Title.Contains(SearchString));
+            }
+            if (!string.IsNullOrEmpty(MovieGenre))
+            {
+                movies = movies.Where(t => t.Genre == MovieGenre);
+            }
+            Movies = await movies.ToListAsync();
+            //检索后需要重新绑定Genres
+            IQueryable<string> genreQuery = from m in _context.Movie orderby m.Genre select m.Genre;
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+
+
+        }
+    }
+}
